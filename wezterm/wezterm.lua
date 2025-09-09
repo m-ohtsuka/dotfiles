ef3b836760d7bfe1b8389c6cc0d8f1c7b854ef08
negistyle <psi-median.1x@icloud.com>
2025-09-09 23:03:59 +0900

    feat(wezterm): Add yen symbol to backslash key mapping
    Update SendKey action syntax to use proper function call format and add
    key mapping for Japanese yen symbol (¥) to output backslash (\)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,14 +5,15 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("PlemolJP Console NF")
-config.initial_cols = 120
+config.initial_cols = 100
 config.initial_rows = 50
 config.font_size = 17.0
 config.color_scheme = "Dracula (Official)"
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
-  { key = "c", mods = "SUPER", action = wezterm.action.SendKey { key = "c", mods = "ALT" } },
-  { key = "x", mods = "SUPER", action = wezterm.action.SendKey { key = "x", mods = "META" } },
+  { key = "c", mods = "SUPER", action = wezterm.action.SendKey({ key = "c", mods = "ALT" }) },
+  { key = "x", mods = "SUPER", action = wezterm.action.SendKey({ key = "x", mods = "META" }) },
+  { key = "¥", action = wezterm.action.SendKey({ key = "\\" }) },
 }
 
 return config
