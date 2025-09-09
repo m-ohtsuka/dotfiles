d169c39fbe319401edff21b81092bf259e0ea9cb
negistyle <psi-median.1x@icloud.com>
2025-09-09 22:35:39 +0900

    feat(wezterm): Add custom key mappings and clean up config
    Add SUPER+c and SUPER+x key bindings to send ALT+c and META+x respectively.
    Remove redundant comments for cleaner configuration.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,17 +4,15 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
 config.font = wezterm.font("PlemolJP Console NF")
-
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 50
-
--- or, changing the font size and color scheme.
 config.font_size = 17.0
 config.color_scheme = "Dracula (Official)"
 config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  { key = "c", mods = "SUPER", action = wezterm.action.SendKey { key = "c", mods = "ALT" } },
+  { key = "x", mods = "SUPER", action = wezterm.action.SendKey { key = "x", mods = "META" } },
+}
 
--- Finally, return the configuration to wezterm:
 return config
