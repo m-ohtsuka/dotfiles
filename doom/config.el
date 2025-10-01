25395d58c7a8b1a20ca42755b7099315cc337668
negistyle <psi-median.1x@icloud.com>
2025-10-01 21:37:36 +0900

    feat(org): Disable visual formatting enhancements
    Remove +pretty flag and disable leading star hiding and startup indentation for cleaner plain text editing experience

--- doom/config.el
+++ doom/config.el
@@ -44,6 +44,9 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setopt org-directory "~/Documents/Org/")
+(after! org
+  (setopt org-hide-leading-stars nil)
+  (setopt org-startup-indented nil))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
