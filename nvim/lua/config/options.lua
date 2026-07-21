8cd0d6aa6602c8435ad06fda6848a14ac6feb46b
negistyle <psi-median.1x@icloud.com>
2026-07-22 07:17:24 +0900

    feat(nvim): LazyVimの初期設定を追加

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.spelllang = { "en", "cjk" }
+vim.opt.relativenumber = false -- 相対表示を無効化
+vim.opt.number = true -- 絶対表示を有効化
